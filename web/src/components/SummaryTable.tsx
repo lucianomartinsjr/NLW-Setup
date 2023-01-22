fcd227fb990d9f68ccc937e649716e8bbdee2a48
Luciano Martins <luciano.martinsjr@hotmail.com>
2023-01-21 23:29:20 -0300

    :heavy_plus_sign: build: adicionado feature no SummaryTable.tsx

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -12,7 +12,9 @@ const weekDays = [
 ];
 
 const summaryDates = generateDatesFromYearBeginning()
-console.log(summaryDates)
+
+const minimumSummaryDatesSize = 18 * 7 // 18weeks
+const amountOdDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
   return (
@@ -33,10 +35,17 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay/>
-            
+          return <HabitDay key={date.toString()}/>
         })}
 
+        {amountOdDaysToFill > 0 && Array.from({ length: amountOdDaysToFill}).map((_,i) =>{
+          return (
+            <div
+              key={i} 
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />  
+          ) 
+        })}
       </div>
     </div>
     
